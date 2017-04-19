6c09552ef00f51e1178135855eff34780ea616e9
zzkluck <zzkluck@outlook.com>
2017-04-19 17:27:19 +0800

    实现了一个二叉树的IEnumerable接口，可是因为某些原因效果跟我想象的不太一样。。。

---
+++ Zzkluck.OBDD.Console/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Zzkluck.OBDD.Library;
+
+namespace Zzkluck.OBDD.Console
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            BinaryTreeBaseNode<int> b6 = new BinaryTreeBaseNode<int>(6, null, null);
+            BinaryTreeBaseNode<int> b7 = new BinaryTreeBaseNode<int>(7, null, null);
+            BinaryTreeBaseNode<int> b9 = new BinaryTreeBaseNode<int>(9, null, null);
+            BinaryTreeBaseNode<int> b8 = new BinaryTreeBaseNode<int>(8, null, b9);
+            BinaryTreeBaseNode<int> b5 = new BinaryTreeBaseNode<int>(5, b8, null);
+            BinaryTreeBaseNode<int> b4 = new BinaryTreeBaseNode<int>(4, null, null);
+            BinaryTreeBaseNode<int> b2 = new BinaryTreeBaseNode<int>(2, b4, b5);
+            BinaryTreeBaseNode<int> b3 = new BinaryTreeBaseNode<int>(3, b6, b7);
+            BinaryTreeBaseNode<int> b1 = new BinaryTreeBaseNode<int>(1, null, b3);
+            BinaryTreeRootNode<int> b0 = new BinaryTreeRootNode<int>(0, b1, b2);
+
+            foreach(BinaryTreeBaseNode<int> b in b0)
+            {
+                System.Console.WriteLine(b.Value);
+            }
+            System.Console.ReadLine();
+        }
+    }
+}
---
+++ Zzkluck.OBDD.Library/BinaryTree.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Zzkluck.OBDD.Library
+{
+    public class BinaryTreeBaseNode<T>
+    {
+        public BinaryTreeBaseNode(T value,BinaryTreeBaseNode<T> left,BinaryTreeBaseNode<T> right, BinaryTreeBaseNode<T> parent=null)
+        {
+            Parent = parent;
+            Left = left;
+            Right = right;
+            Value = value;
+        }
+        public T Value;
+        public BinaryTreeBaseNode<T> Parent;
+        public BinaryTreeBaseNode<T> Left;
+        public BinaryTreeBaseNode<T> Right;
+        public virtual bool isRoot() { return Parent != null; }
+        public virtual bool isEnd() { return Left == null && Right == null; }
+    }
+
+    public sealed class BinaryTreeRootNode<T> : BinaryTreeBaseNode<T>, IEnumerable
+    {
+        public BinaryTreeRootNode(T value, BinaryTreeBaseNode<T> left, BinaryTreeBaseNode<T> right, BinaryTreeBaseNode<T> parent = null) 
+            : base(value, left, right, parent)
+        {
+        }
+        public override bool isRoot() { return true; }
+        public IEnumerator GetEnumerator()
+        {
+            return PreorderTraversal();
+        }
+
+        public IEnumerator PreorderTraversal()
+        {
+            Stack<BinaryTreeBaseNode<T>> traversalStack = new Stack<BinaryTreeBaseNode<T>>();
+            BinaryTreeBaseNode<T> nowTraveling = this;
+
+            traversalStack.Push(nowTraveling);
+            while (traversalStack.Count != 0)
+            {
+                while (nowTraveling != null)
+                {
+                    traversalStack.Push(nowTraveling);
+                    yield return nowTraveling;
+                    nowTraveling = nowTraveling.Left;
+                }
+                nowTraveling = traversalStack.Pop().Right;
+            }
+        }
+        public IEnumerator<object> BreadthFirst()
+        { 
+            Queue<BinaryTreeBaseNode<T>> traveralQueue = new Queue<BinaryTreeBaseNode<T>>();
+            traveralQueue.Enqueue(this);
+
+            while(traveralQueue.Count!=0)
+            {
+                BinaryTreeBaseNode<T> node = traveralQueue.Dequeue();
+                yield return node;
+                if (node.Left != null)
+                {
+                    traveralQueue.Enqueue(node.Left);
+                }
+                if (node.Right != null)
+                {
+                    traveralQueue.Enqueue(node.Right);
+                }
+            }
+        }
+
+
+
+    }
+
+}
